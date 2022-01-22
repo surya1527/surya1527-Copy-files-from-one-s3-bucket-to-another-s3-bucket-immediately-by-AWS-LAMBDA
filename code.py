73956644347c20741907c2c6bb2239c5a77cb68f
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-01-22 11:50:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,8 @@ def lambda_handler(event, context):
     file_name = event['Records'][0]['s3']['object']['key'];
     service_name='s3'
     region_name='ap-south-1'
-    aws_access_key_id='AKIATIRE3MO3ZYYUK23C'
-    aws_secret_access_key='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'
+    aws_access_key_id='Access Key'
+    aws_secret_access_key='Secret access key'
     s3 = boto3.resource(
         service_name=service_name,
         region_name=region_name,
